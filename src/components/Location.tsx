21b23bf2a812b27b388cc6a1edf4621e7c07360a
pogist <murilo.paixao.2@gmail.com>
2024-04-09 16:07:46 -0300

    refactor: remove current location lable

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -7,22 +7,17 @@ import { capitalize } from '../util';
 type LocationProps = {
   city: string;
   containerStyle?: StyleProp<ViewStyle>;
-  currentLocation?: boolean;
   date: string;
 };
 
 export default function Location({
   city,
   containerStyle,
-  currentLocation,
   date,
 }: LocationProps) {
   const styles = useStyles(themedStyles);
   return (
     <View style={[styles.container, containerStyle ?? {}]}>
-      {currentLocation && (
-        <Text style={styles.currentLocationLabel}>Localização atual</Text>
-      )}
       <View style={styles.cityContainer}>
         <Text style={styles.city}>{capitalize(city)}</Text>
         <Text style={styles.date}>{format(date)}</Text>
@@ -47,12 +42,6 @@ const themedStyles = createStyles((theme) =>
     container: {
       gap: spacing(3),
       alignItems: 'center',
-    },
-    currentLocationLabel: {
-      color: theme.color.orange,
-      fontSize: spacing(3),
-      lineHeight: spacing(3),
-      fontWeight: '500',
     },
     date: {
       color: theme.color.secondaryLabel,
